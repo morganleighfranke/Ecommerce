fb62b03a6b5c680f113b8be58bca7e6ffa563b66
Morgan Franke <morganleighfranke@gmail.com>
2021-04-19 10:53:58 -0400

    started routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productDataId = await Product.findByPk(req.params.id, {
+      include: [{ model: Tag, through: ProductTag, foreignKey: 'product_id'}]
+    });
+    res.status(200).json(productDataId);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
