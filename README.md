05022c6feb3ed26d957807f7a78266fdda0acb7e
Morgan Franke <morganleighfranke@gmail.com>
2021-04-20 11:51:29 -0400

    video walkthrough and updated routes

--- README.md
+++ README.md
@@ -17,6 +17,8 @@
 
   ## Video Walk through
   Link to video: 
+  1. Creating database, seeding database, starting server, and returning GET routes for products, categories and tags in Insomnia Core: https://drive.google.com/file/d/1llLIEui46YE3PKiPY9wbX0RkGaP5_50F/view
+  2. GET routes for returning a single category, single product, and single tag in Insomnia Core: https://drive.google.com/file/d/19N8AyhPk48d5rTW9QKEY5cFgtEIfQnVM/view
 
   ## Contact me
   Github profile: https://github.com/morganleighfranke
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll();
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagDataById = await Tag.findByPk( req.params.id, {
+      include: [{model: Product, foreignKey: 'tag_id'}]
+    });
+    res.status(200).json(tagDataById);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const createTag = await Tag.create(req.body);
+    res.status(200).json(createTag);
+  } catch (error){
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updateTag[0]) {
+      res.status(404).json({ message: 'There is no tag with this id'});
+      return;
+    }
+    res.status(200).json(updateTag);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deleteTag);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
